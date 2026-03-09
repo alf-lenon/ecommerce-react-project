2488902835031704d0d3b3e45370786f489a712b
Alf <sample@example.com>
2026-03-09 10:41:23 +0800

    Move checkout page into React

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ import './HomePage.css';
 function HomePage() {
 	return (
 		<>
-			(
+			<title>Ecommerce Project</title>
 			<div className='header'>
 				<div className='left-section'>
 					<a href='index.html' className='header-link'>
@@ -26,7 +26,7 @@ function HomePage() {
 						<span className='orders-text'>Orders</span>
 					</a>
 
-					<a className='cart-link header-link' href='checkout.html'>
+					<a className='cart-link header-link' href='checkout'>
 						<img className='cart-icon' src='images/icons/cart-icon.png' />
 						<div className='cart-quantity'>3</div>
 						<div className='cart-text'>Cart</div>
@@ -183,7 +183,6 @@ function HomePage() {
 					</div>
 				</div>
 			</div>
-			)
 		</>
 	);
 }
