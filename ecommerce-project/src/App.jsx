e4caf083f8ccfe403feb77cbe3e83eebc2776fa9
Alf <sample@example.com>
2026-03-09 10:21:17 +0800

    Create multiple pages using react-router

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,12 +1,15 @@
+import { Routes, Route } from 'react-router';
 import HomePage from './pages/HomePage';
-import CheckOut from './pages/CheckOut';
+
 import './App.css';
 
 function App() {
 	return (
 		<>
-			<HomePage />
-			<CheckOut />
+			<Routes>
+				<Route index element={<HomePage />} />
+				<Route path='checkout' element={<div>Test Checkout Page</div>} />
+			</Routes>
 		</>
 	);
 }
